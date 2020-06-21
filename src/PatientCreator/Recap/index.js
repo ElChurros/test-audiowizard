4838d595d02529c88e16c3c6e2f319d431638f60
Arthur Lemaire <arthur.lemaire@epitech.eu>
2020-06-22 00:08:04 +0200

    Center recap

--- src/PatientCreator/Recap/index.js
+++ src/PatientCreator/Recap/index.js
@@ -5,7 +5,9 @@ import Context from '../../context';
 export function Recap({className, ...props}) {
   const context = useContext(Context);
   return (
-    <h1 className={`my-md-5 ${className ? className : ''}`}>{`${context.patient.sex === 'M' ? 'Monsieur ' : context.patient.sex === 'F' ? 'Madame ' : ''}${context.patient.firstName} ${context.patient.lastName}`}</h1>
+    <div className={`d-flex h-100 flex-column justify-content-center align-items-center ${className ? className : ''}`}>
+      <h1 className='my-md-5'>{`${context.patient.sex === 'M' ? 'Monsieur ' : context.patient.sex === 'F' ? 'Madame ' : ''}${context.patient.firstName} ${context.patient.lastName}`}</h1>
+    </div>
   );
 };
 
