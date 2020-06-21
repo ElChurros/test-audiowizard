10b139d4f01ac40064a8e45e3ef5d04351f93cd9
Arthur Lemaire <arthur.lemaire@epitech.eu>
2020-06-21 21:48:07 +0200

    Add context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Context from './context';
 import './App.css';
+
 
 function App() {
   return (
-    <>
-    </>
+    <Context.Provider>
+    </Context.Provider>
   );
 }
 
