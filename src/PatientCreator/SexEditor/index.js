dc6f79c52c87d43adc1a74728abf0a9b47d2b172
Arthur Lemaire <arthur.lemaire@epitech.eu>
2020-06-22 00:08:39 +0200

    fix SexEditor layout

--- src/PatientCreator/SexEditor/index.js
+++ src/PatientCreator/SexEditor/index.js
@@ -5,11 +5,9 @@ import Context from '../../context';
 export function SexEditor({className, nextStep, ...props}) {
   const context = useContext(Context);
   return (
-    <div className='container-fluid'>
-      <div className='row d-flex justify-content-around'>
-        <button onClick={e => {context.patient.setSex('M'); nextStep()}} className='btn btn-light btn-lg'>Homme</button>
-        <button onClick={e => {context.patient.setSex('F'); nextStep()}} className='btn btn-light btn-lg'>Femme</button>
-      </div>
+    <div className={`row d-flex justify-content-around align-items-center h-100 ${className}`}>
+      <button onClick={e => {context.patient.setSex('M'); nextStep()}} className='btn btn-light btn-lg'>Homme</button>
+      <button onClick={e => {context.patient.setSex('F'); nextStep()}} className='btn btn-light btn-lg'>Femme</button>
     </div>
   );
 }
